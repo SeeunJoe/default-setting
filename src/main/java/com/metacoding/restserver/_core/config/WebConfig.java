72d5e54c63a49e37d7d53d359674f3792832bbfc
jse <taxsilver331@gmail.com>
2025-01-08 11:09:58 +0900

    default setting ver3

--- src/main/java/com/metacoding/restserver/_core/config/WebConfig.java
+++ src/main/java/com/metacoding/restserver/_core/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.metacoding.restserver._core.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
@@ -12,12 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
         // 1. 절대경로 file:///c:/upload/
         // 2. 상대경로 file:./upload/
         registry
-                .addResourceHandler("/upload/**") // html에서 경로를 적으면
+                .addResourceHandler("/images/**") // html에서 경로를 적으면
                 .addResourceLocations("file:" + "./images/") // 웹서버의 /images/ 폴더 경로를 찾음
                 .setCachePeriod(60 * 60); // 초 단위 => 한시간
 
         // file://localhost/머시기/upload/images/
 
     }
-
 }
